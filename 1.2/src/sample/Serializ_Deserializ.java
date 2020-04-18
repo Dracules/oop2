789f6d150867ed9fb4539ef3a34494085103afc8
dracules <draculessi23@gmail.com>
2020-04-18 14:53:38 +0300

    Initial Commit

---
+++ 1.2/src/sample/Serializ_Deserializ.java
@@ -0,0 +1,8 @@
+package sample;
+
+import java.util.ArrayList;
+
+abstract class Serializ_Deserializ {
+    abstract public void Save(ArrayList<learner> arr);
+    abstract public void Load(ArrayList<learner> arr);
+}
